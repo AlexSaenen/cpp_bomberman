9cc8bd9812ceafa66b47ac6a29d3dd984a394956
saenen_a <alexander.saenen@epitech.eu>
2015-06-13 23:00:25 +0200

    "commitwork"

--- ApplicationFiles/Modules/GameModule.cpp
+++ ApplicationFiles/Modules/GameModule.cpp
@@ -5,7 +5,7 @@
 // Login   <saenen_a@epitech.net>
 // 
 // Started on  Tue May 19 11:00:44 2015 Alexander Saenen
-// Last update Sat Jun 13 22:59:37 2015 Alexander Saenen
+// Last update Sat Jun 13 23:00:22 2015 Alexander Saenen
 //
 
 #include <GameModule.hh>
@@ -63,9 +63,9 @@ void					GameModule::pushOnMap(GameObject *object) {
     }
     double	x = shape->getPosX();
     double	y = shape->getPosY();
-    x = x > 0 ? ((x + 2.5) / 2.5) : x / 2.5;
-    y = y > 0 ? ((y + 2.5) / 2.5) : y / 2.5;
-    std::cout << "PUSH " << x << " " << y << " " << shape->getPosX() << " " << shape->getPosY() << std::endl;
+    // x = x > 0 ? ((x + 2.5) / 2.5) : x / 2.5;
+    // y = y > 0 ? ((y + 2.5) / 2.5) : y / 2.5;
+    // std::cout << "PUSH " << x << " " << y << " " << shape->getPosX() << " " << shape->getPosY() << std::endl;
     if (shape == NULL)
       throw LogicException("GameObject hasn't got a shape.");
     if (_gameMap.find(x) == _gameMap.end())
@@ -80,11 +80,11 @@ void					GameModule::pushOnMap(GameObject *object) {
 }
 
 void                                    GameModule::pushOnMap(const double x, const double y, const GameObject::ObjectType type) {
-  int	_x = x > 0 ? (x + 2.5) / 2.5 : x / 2.5;
-  int	_y = y > 0 ? (y + 2.5) / 2.5 : y / 2.5;
-  std::cout << "PUSH " << _x << " " << _y << " " << x << " " << y << std::endl;
-  // int _x = x;
-  // int _y = y;
+  // int	_x = x > 0 ? (x + 2.5) / 2.5 : x / 2.5;
+  // int	_y = y > 0 ? (y + 2.5) / 2.5 : y / 2.5;
+  // std::cout << "PUSH " << _x << " " << _y << " " << x << " " << y << std::endl;
+  int _x = x;
+  int _y = y;
   if (_gameMap.find(_x) == _gameMap.end())
     _gameMap[_x] = std::map<int, std::list<GameObject::ObjectType> >();
   ((_gameMap[_x])[_y]).push_back(type);
@@ -106,9 +106,9 @@ void						GameModule::popOnMap(GameObject *object) {
     }
     double	x = shape->getPosX();
     double	y = shape->getPosY();
-    x = x > 0 ? ((x + 2.5) / 2.5) : x / 2.5;
-    y = y > 0 ? ((y + 2.5) / 2.5) : y / 2.5;
-    std::cout << "POP " << x << " " << y << " " << shape->getPosX() << " " << shape->getPosY() << std::endl;
+    // x = x > 0 ? ((x + 2.5) / 2.5) : x / 2.5;
+    // y = y > 0 ? ((y + 2.5) / 2.5) : y / 2.5;
+    // std::cout << "POP " << x << " " << y << " " << shape->getPosX() << " " << shape->getPosY() << std::endl;
     if (shape == NULL)
       throw LogicException("GameObject hasn't got a shape.");
     if (_gameMap.find(x) == _gameMap.end())
