100544a97a58fc58c75f56bcce9d035e3c5567ba
mortau_g <gregoire.mortaud@epitech.eu>
2015-06-13 15:58:24 +0200

    LuaScript Object

--- Script/main_test.cpp
+++ Script/main_test.cpp
@@ -1,10 +1,16 @@
-#include "lua_basics.hh"
+#include "../Includes/lua_basics.hh"
 
 int	main()
 {
   LuaScript	*lua;
 
   lua = new LuaScript();
+  lua->DoScript();
+  lua->PullFunc("run_ia");
+  lua->PushInteger(1);
+  lua->PushInteger(2);
+  lua->PushInteger(3);
+  lua->Pcall(3, 1, 0);
   delete lua;
   return (0);
 }
